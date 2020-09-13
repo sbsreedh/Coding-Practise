826d803e762e1da90dc44a748a0e1c67e94f57b5
Sonali B Sreedhar <50754966+sbsreedh@users.noreply.github.com>
2020-09-12 22:44:50 -0700

    Update topKFrequent.py

--- topKFrequent.py
+++ topKFrequent.py
@@ -17,4 +17,22 @@ class Solution:
         sorted_arr=sorted(hashMap.keys(),key=lambda x:(-hashMap[x],x))
         print(sorted_arr)
         return sorted_arr[:k]
+    
+#heap Solution:
+#TC:O(Nlogk), N-elements, k elements in heap
+#SC:O(N)
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        #initialize a max heap
+        #create counter dict to store freq of words
+        #iterate through the counter and add with  -count and keys
+        #pop from heap till k elements
+        heap=[]
+        count=collections.Counter(words)
+        for key,value in count.items():
+            heapq.heappush(heap,(-value,key))
+        return [heapq.heappop(heap)[1] for _ in range(k)]
+     
+#        
         
