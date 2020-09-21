e44ab9d86e89906e6de13fd433f67c07b477bb67
Sonali B Sreedhar <50754966+sbsreedh@users.noreply.github.com>
2020-09-21 10:36:40 -0700

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -3,7 +3,7 @@
 #1.Create a hashmap with {nums[i]:i}
 #2.Search in the hashmap fo rthe complement (target-nums[i]) and ensure it is no tthe same num and return the answer.
 
-#One pass solution
+#Two pass solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         res=[]
@@ -13,6 +13,16 @@ class Solution:
         for i in range(len(nums)):
             if target-nums[i] in hashMap and hashMap[target-nums[i]]!=i:
                 return (i,hashMap[target-nums[i]])
-                
+#One pass solution
+#TC:O(N)
+#SC:O(N)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d=defaultdict(int)
+        for i, num in enumerate(nums):
+            if target-num in d:
+                    return d[target-num], i
+            d[num]=i
        
             
